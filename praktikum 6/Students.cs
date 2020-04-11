b95c54c35c1a64cbe3bc243e0f1485bc61f35371
immanuel21 <immanuel.manafe@students.amikom.ac.id>
2020-04-11 11:47:21 +0700

    first commit

---
+++ praktikum 6/Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace praktikum_6
+{
+    public class Students : Person
+    {
+        public Students(string name, int age, string studentsid, string email) : base(name, age)
+        {
+            Name = name;
+            Age = age;
+            StudentsId = studentsid;
+            Email = email;
+        }
+        public string StudentsId { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ praktikum 6/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace praktikum_6
+{
+    public class Teacher : Person
+    {
+        public Teacher(string name, int age, string teacherid, string subject) : base(name, age)
+        {
+            Name = name;
+            Age = age;
+            TeacherId = teacherid;
+            Subject = subject;
+        }
+
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+    }
+}
